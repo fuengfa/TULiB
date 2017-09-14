2ee2bce4ffd1be976180e44c23bf2d37fede7b3a
FloorFueng <fuengfa1996@gmail.com>
2017-09-14 18:16:24 +0700

    this is a library

--- TULiB/docmanager/admin.py
+++ TULiB/docmanager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from docmanager.models import*
 # Register your models here.
+exclude_document_filter = ['ISBN','title','id']
+
+class AuthorAdmin(admin.ModelAdmin):
+	list_display_links = None
+	list_display = ['firstname','lastname']
+	list_filter = ['firstname','lastname']
+	list_editable = ['firstname','lastname']
+	
+class DocumentAdmin(admin.ModelAdmin):
+	list_display_links = None
+	list_display = [f.name for f in Document._meta.fields]
+	list_filter = [f.name for f in Document._meta.fields if not f.name in exclude_document_filter]
+	list_editable = [f.name for f in Document._meta.fields]
+
+admin.site.register(Document,DocumentAdmin)
+admin.site.register(Author,AuthorAdmin)
--- TULiB/docmanager/models.py
+++ TULiB/docmanager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    def __str__(self):
+    	return "{} {}".format(self.firstname,self.lastname)
+
+class Document(models.Model):
+    choice = [(0,"Book"),(1,"Thesis")]
+    title = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=14)
+    release_date = models.DateField()
+    author = models.ForeignKey(Author)
+    doc_type = models.IntegerField(choices=choice)
